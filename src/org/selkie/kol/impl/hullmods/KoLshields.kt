186db5d8911a106b0de4277d325918a0c3fd71dd
Nia Tahl <eliyanbrize@gmail.com>
2023-06-18 20:16:15 +0200

    KoL shields

---
+++ src/org/selkie/kol/impl/hullmods/KoLshields.kt
@@ -0,0 +1,13 @@
+package org.selkie.kol.impl.hullmods
+
+import com.fs.starfarer.api.combat.BaseHullMod
+import com.fs.starfarer.api.combat.ShipAPI
+
+class KoLshields : BaseHullMod() {
+
+    private val INNERLARGE = "graphics/fx/kol_shield_fx.png"
+    private val OUTERLARGE = "graphics/fx/kol_shield_fx.png"
+    override fun applyEffectsAfterShipCreation(ship: ShipAPI, id: String) {
+        ship.shield.setRadius(ship.shieldRadiusEvenIfNoShield, INNERLARGE, OUTERLARGE)
+    }
+}
