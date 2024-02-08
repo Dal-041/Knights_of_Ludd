450de3870a562f89578a4447f8a53c14f590e044
DalAux <dalauxiliary@gmail.com>
2024-02-08 14:12:17 -0800

    Added the path to settings, should function now

--- src/org/selkie/kol/impl/hullmods/DuskyShields3.kt
+++ src/org/selkie/kol/impl/hullmods/DuskyShields3.kt
@@ -10,7 +10,7 @@ class DuskyShields3 : BaseHullMod() {
     private val INNERLARGE2 = "data/strings/com/fs/starfarer/api/impl/campaign/you can hear it cant you/our whispers through the void/our song/graphics/fx/zea_shield_elysia_2.png"
     private val OUTERLARGE2 = "data/strings/com/fs/starfarer/api/impl/campaign/you can hear it cant you/our whispers through the void/our song/graphics/fx/zea_shield_elysia_2.png"
 
-    private final val conformalShieldsID = "kol_conformal_shield"
+    private val conformalShieldsID = "kol_conformal_shield"
 
     override fun applyEffectsAfterShipCreation(ship: ShipAPI, id: String) {
         if (ship.shield != null) {
