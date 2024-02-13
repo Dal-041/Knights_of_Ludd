9d2a650b7a4b211c51f9975e42215eee565fa26b
DalAux <dalauxiliary@gmail.com>
2024-02-12 18:00:55 -0800

    KOL hull AI biaser, potentially working

---
+++ src/org/selkie/kol/helpers/KOLUtils.java
@@ -0,0 +1,22 @@
+package org.selkie.kol.helpers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class KOLUtils {
+    public static final String[] knightsShips = {
+            "kol_alysse",
+            "kol_larkspur",
+            "kol_libra",
+            "kol_lotus",
+            "kol_lunaria",
+            "kol_marigold",
+            "kol_mimosa",
+            "kol_protea",
+            "kol_snowdrop",
+            "kol_sundew",
+            "kol_tamarisk"
+    };
+    public static final ArrayList<String> knightsShipsList = new ArrayList<>(Arrays.asList(knightsShips));
+}
---
+++ src/org/selkie/kol/plugins/KOL_ShipAITweaker.java
@@ -0,0 +1,70 @@
+package org.selkie.kol.plugins;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.PluginPick;
+import com.fs.starfarer.api.campaign.CampaignPlugin;
+import com.fs.starfarer.api.combat.ShipAIConfig;
+import com.fs.starfarer.api.combat.ShipAIPlugin;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import com.fs.starfarer.api.impl.campaign.ids.Personalities;
+import org.selkie.kol.helpers.KOLUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KOL_ShipAITweaker {
+    public static PluginPick<ShipAIPlugin> pickShipAI(FleetMemberAPI member, ShipAPI ship) {
+        if (ship.isFighter()) return null;
+        String hullId = ship.getHullSpec().getHullId();
+
+        if (!KOLUtils.knightsShipsList.contains(hullId)) return null;
+
+        List<String> supportHulls = new ArrayList<>(2);
+        supportHulls.add("kol_mimosa");
+        supportHulls.add("kol_lotus");
+        List<String> assaultHulls = new ArrayList<>(2);
+        assaultHulls.add("kol_larkspur");
+        assaultHulls.add("kol_alysse");
+
+        //HullSize size = ship.getHullSize();
+
+        ShipAIConfig config = new ShipAIConfig();
+        config.alwaysStrafeOffensively = true;
+        config.backingOffWhileNotVentingAllowed = true;
+        config.turnToFaceWithUndamagedArmor = true;
+        config.burnDriveIgnoreEnemies = false;
+
+        boolean carrier = false;
+        boolean support = false;
+        boolean assault = false;
+        if (ship.getVariant() != null) {
+            carrier = ship.getVariant().isCarrier() && !ship.getVariant().isCombat();
+        }
+        if (supportHulls.contains(hullId)) {
+            support = true;
+        }
+        if (assaultHulls.contains(hullId)) {
+            assault = true;
+        }
+
+        //Bump toward target personality 1 step
+        if (ship.getCaptain() != null) {
+            if (carrier || support) {
+                if (ship.getCaptain().isDefault() || ship.getCaptain().isPlayer()) config.personalityOverride = Personalities.CAUTIOUS;
+                else if (ship.getCaptain().getPersonalityAPI().getId().equals(Personalities.RECKLESS)) config.personalityOverride = Personalities.AGGRESSIVE;
+                else if (ship.getCaptain().getPersonalityAPI().getId().equals(Personalities.AGGRESSIVE)) config.personalityOverride = Personalities.STEADY;
+                else if (ship.getCaptain().getPersonalityAPI().getId().equals(Personalities.STEADY)) config.personalityOverride = Personalities.CAUTIOUS;
+                else if (ship.getCaptain().getPersonalityAPI().getId().equals(Personalities.TIMID)) config.personalityOverride = Personalities.CAUTIOUS;
+            } else if (assault) {
+                if (ship.getCaptain().isDefault() || ship.getCaptain().isPlayer()) config.personalityOverride = Personalities.AGGRESSIVE;
+                else if (ship.getCaptain().getPersonalityAPI().getId().equals(Personalities.CAUTIOUS)) config.personalityOverride = Personalities.STEADY;
+                else if (ship.getCaptain().getPersonalityAPI().getId().equals(Personalities.AGGRESSIVE)) config.personalityOverride = Personalities.RECKLESS;
+                else if (ship.getCaptain().getPersonalityAPI().getId().equals(Personalities.STEADY)) config.personalityOverride = Personalities.AGGRESSIVE;
+                else if (ship.getCaptain().getPersonalityAPI().getId().equals(Personalities.TIMID)) config.personalityOverride = Personalities.CAUTIOUS;
+            }
+        }
+
+        return new PluginPick<ShipAIPlugin>(Global.getSettings().createDefaultShipAI(ship, config), CampaignPlugin.PickPriority.MOD_SPECIFIC);
+    }
+}
