a0c15859ac5771025aeb9a5db7a79c9283e7fb51
scardwell <scardwell15@gmail.com>
2024-02-16 11:47:03 -0700

    - amaterasu effect fixes

--- src/org/selkie/kol/impl/combat/activators/RadianceActivator.kt
+++ src/org/selkie/kol/impl/combat/activators/RadianceActivator.kt
@@ -2,9 +2,9 @@ package org.selkie.kol.impl.combat.activators
 
 import activators.CombatActivator
 import com.fs.starfarer.api.Global
-import com.fs.starfarer.api.combat.*
-import com.fs.starfarer.api.combat.listeners.ApplyDamageResultAPI
-import com.fs.starfarer.api.loading.DamagingExplosionSpec
+import com.fs.starfarer.api.combat.CombatEntityAPI
+import com.fs.starfarer.api.combat.DamageType
+import com.fs.starfarer.api.combat.ShipAPI
 import com.fs.starfarer.api.util.IntervalUtil
 import com.fs.starfarer.api.util.Misc
 import com.fs.starfarer.api.util.Misc.ZERO
@@ -12,7 +12,10 @@ import org.dark.shaders.distortion.DistortionShader
 import org.dark.shaders.distortion.RippleDistortion
 import org.dark.shaders.light.LightShader
 import org.dark.shaders.light.StandardLight
+import org.lazywizard.lazylib.MathUtils
+import org.lazywizard.lazylib.combat.CombatUtils
 import org.lwjgl.util.vector.Vector2f
+import org.magiclib.kotlin.setAlpha
 import org.magiclib.util.MagicRender
 import org.selkie.kol.combat.ParticleData
 import org.selkie.kol.impl.hullmods.CoronalCapacitor
@@ -22,21 +25,17 @@ import java.io.IOException
 
 
 class RadianceActivator(ship: ShipAPI?) : CombatActivator(ship) {
-    private val PARTICLE_INTERVAL = IntervalUtil(0.05f, 0.1f)
+    private val PARTICLE_INTERVAL = IntervalUtil(1f, 1f)
     private var dummyMine: CombatEntityAPI? = null
 
-    private var waveCnt = 0;
-    private var rotation = 0f
-    private var opacity = 0f
     private val spriteRing = Global.getSettings().getSprite("fx", "zea_ring_targeting")
-    private val SPRITE_PATH = ""
 
     private val glowColorFull = Color(200,150,50, 200)
     private val glowColorEmpty = Color(255,100,25, 0)
 
     private val EFFECT_RANGE =
-        DAMAGE_RANGE-(DAMAGE_RANGE*0.05f) //"For "player visual purposes" we reduce it by 5% See some random player theory crap."
-    private val effectSize = Vector2f(EFFECT_RANGE*2, EFFECT_RANGE*2)
+        DAMAGE_RANGE - (DAMAGE_RANGE * 0.05f) //"For "player visual purposes" we reduce it by 5% See some random player theory crap."
+    private val effectSize = Vector2f(EFFECT_RANGE * 2, EFFECT_RANGE * 2)
 
     override fun canAssignKey(): Boolean {
         return false
@@ -77,71 +76,76 @@ class RadianceActivator(ship: ShipAPI?) : CombatActivator(ship) {
     }
 
     override fun onActivate() {
-        if (ship.isAlive) {
-            val glowColorNow = Misc.interpolateColor(glowColorEmpty, glowColorFull, ship.customData[CoronalCapacitor.CAPACITY_FACTOR_KEY] as Float)
-            Global.getCombatEngine().addSmoothParticle(ship.location, ZERO, DAMAGE_RANGE*4f, 1f, 0.1f, glowColorNow)
-        }
-
         //spawn an explosion with no effects
         val explosionDamage = getDamage()
-        val explosionSpec = DamagingExplosionSpec(
-            0.5f,
-            DAMAGE_RANGE,
-            DAMAGE_RANGE,
-            explosionDamage,
-            explosionDamage,
-            CollisionClass.PROJECTILE_NO_FF,
-            CollisionClass.PROJECTILE_FIGHTER,
-            1f,
-            2f,
-            0.33f,
-            0,
-            Color(255, 200, 30, 0),
-            null
-        )
-        explosionSpec.damageType = DamageType.HIGH_EXPLOSIVE
-        explosionSpec.isShowGraphic = false
-        explosionSpec.effect = RadianceOnHitEffect()
-        Global.getCombatEngine().spawnDamagingExplosion(explosionSpec, ship, ship.location, false)
 
-        if (KOL_ModPlugin.hasGraphicsLib) {
-            if (waveCnt % 20 == 0) {
-                val ripple = RippleDistortion(ship.location, Vector2f())
-                ripple.size = DAMAGE_RANGE / 1.2f
-                ripple.intensity = 5f
-                ripple.frameRate = 60f
-                ripple.fadeInSize(1f)
-                ripple.fadeOutIntensity(2f)
-                ripple.velocity = Vector2f(1f,1f)
-                DistortionShader.addDistortion(ripple)
-
-                val light = StandardLight(ship.location, Vector2f(), Vector2f(), null)
-                light.size = DAMAGE_RANGE / 1.1f
-                light.intensity = 2f
-                light.setLifetime(0.66f)
-                light.autoFadeOutTime = 1f
-                light.setColor(Color(255, 125, 25, 255))
-                LightShader.addLight(light)
+        CombatUtils.getEntitiesWithinRange(ship.location, DAMAGE_RANGE)
+            .filterNot { it.owner == ship.owner }
+            .map { it to 1f - MathUtils.getDistance(ship, it).coerceAtLeast(ship.collisionRadius) / DAMAGE_RANGE }
+            .forEach { (target, rangeRatio) ->
+                val effectiveDamage = rangeRatio * explosionDamage
+                val bypassShields = target.shield == null || target.shield.activeArc < 360f
+                Global.getCombatEngine().applyDamage(
+                    target,
+                    target.location,
+                    effectiveDamage,
+                    DamageType.HIGH_EXPLOSIVE,
+                    effectiveDamage,
+                    bypassShields,
+                    false,
+                    ship,
+                    false
+                )
             }
-
-            waveCnt++
-        }
     }
 
     override fun advance(amount: Float) {
         if (ship.customData[CoronalCapacitor.CAPACITY_FACTOR_KEY] == null) return; //Hopefully just one frame
+
+        val glowColorNow = Misc.interpolateColor(glowColorEmpty, glowColorFull, ship.customData[CoronalCapacitor.CAPACITY_FACTOR_KEY] as Float)
         if (!Global.getCombatEngine().isUIShowingHUD || Global.getCombatEngine().isUIShowingDialog || Global.getCombatEngine().combatUI.isShowingCommandUI) {
             return;
         } else {
             if (!ship.isHulk && !ship.isPiece && ship.isAlive) {
-                val glowColorNow = Misc.interpolateColor(glowColorEmpty, glowColorFull, ship.customData[CoronalCapacitor.CAPACITY_FACTOR_KEY] as Float)
                 MagicRender.singleframe(spriteRing, ship.location, effectSize, 0f, glowColorNow, true)
+            }
+        }
+
+        if (ship.isAlive) {
+            Global.getCombatEngine()
+                .addSmoothParticle(ship.location, ZERO, DAMAGE_RANGE * 4f, 0.25f, 0.01f * amount, glowColorNow.setAlpha(
+                    (glowColorNow.alpha * 0.7f).toInt()
+                ))
+
+            if (KOL_ModPlugin.hasGraphicsLib) {
+                PARTICLE_INTERVAL.advance(amount)
+                if (PARTICLE_INTERVAL.intervalElapsed()) {
+                    val light = StandardLight(Vector2f(), Vector2f(), Vector2f(), null)
+                    light.size = DAMAGE_RANGE / 1.1f
+                    light.intensity = 0.33f
+                    light.setLifetime(0.66f)
+                    light.autoFadeOutTime = 1f
+                    light.setColor(Color(255, 125, 25))
+                    light.location = ship.location
+                    LightShader.addLight(light)
+
+                    val ripple = RippleDistortion(Vector2f(), Vector2f())
+                    ripple.size = DAMAGE_RANGE / 1.075f
+                    ripple.intensity = 5f
+                    ripple.frameRate = 60f
+                    ripple.fadeInSize(1f)
+                    ripple.fadeOutIntensity(2f)
+                    ripple.velocity = Vector2f(1f, 1f)
+                    ripple.location = ship.location
+                    DistortionShader.addDistortion(ripple)
+                }
             }
         }
 
         if (canActivate()) {
             if (dummyMine == null) {
-                dummyMine = Global.getCombatEngine().spawnProjectile(ship, null, "zea_radiance_dummy_wpn", ship.location, 0f, Vector2f())
+                dummyMine = Global.getCombatEngine()
+                    .spawnProjectile(ship, null, "zea_radiance_dummy_wpn", ship.location, 0f, Vector2f())
             } else {
                 dummyMine!!.location.set(ship.location)
             }
@@ -235,33 +239,12 @@ class RadianceActivator(ship: ShipAPI?) : CombatActivator(ship) {
         }
     }
 
-    override fun init() {
-        super.init()
-        if (spriteRing == null) {
-            // Load sprite if it hasn't been loaded yet - not needed if you add it to settings.json
-            try {
-                Global.getSettings().loadTexture(SPRITE_PATH)
-            } catch (ex: IOException) {
-                throw RuntimeException("Failed to load sprite '$SPRITE_PATH'!", ex)
-            }
-        }
+    override fun getStateText(): String {
+        return ""
     }
 
     override fun getBarFill(): Float {
-        return 1f
-    }
-
-    private inner class RadianceOnHitEffect : OnHitEffectPlugin {
-        override fun onHit(
-            projectile: DamagingProjectileAPI?,
-            target: CombatEntityAPI,
-            point: Vector2f,
-            shieldHit: Boolean,
-            damageResult: ApplyDamageResultAPI,
-            engine: CombatEngineAPI
-        ) {
-            damageResult.empDamage = getDamage()
-        }
+        return ((ship.customData[CoronalCapacitor.CAPACITY_FACTOR_KEY] as Float? ?: 0f) / 0.5f).coerceIn(0f..1f)
     }
 
     companion object {
