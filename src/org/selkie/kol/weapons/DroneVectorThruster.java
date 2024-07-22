e24f647952e6acf12fcb1386b3c3030a89ea1ba0
Starficz <starficz@gmail.com>
2024-07-22 19:21:15 -0400

    Ao Drone Thrusters

--- src/org/selkie/kol/weapons/DroneVectorThruster.java
+++ src/org/selkie/kol/weapons/DroneVectorThruster.java
@@ -3,178 +3,133 @@ package org.selkie.kol.weapons;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.*;
 import com.fs.starfarer.api.util.Misc;
-import org.lazywizard.lazylib.FastTrig;
 import org.lazywizard.lazylib.MathUtils;
-import org.lazywizard.lazylib.VectorUtils;
-import org.lwjgl.util.vector.Vector2f;
 
 public class DroneVectorThruster implements EveryFrameWeaponEffectPlugin {
-    private boolean runOnce = false, accel = false, turn = false;
-    private ShipAPI ship;
-    private ShipAPI thrusterDrone;
-    private ShipEngineControllerAPI.ShipEngineAPI thruster;
-    private ShipEngineControllerAPI engines;
-    private float previousThrust = 0;
 
-    //Smooth thrusting prevents instant changes in directions and levels of thrust, lower is smoother
-    private float maxThrustChangePerSecond = 0;
-    private float maxAngleChangePerSecond = 0;
-    private float turnRightAngle = 0, thrustToTurn = 0, neutralAngle = 0, wobbleOffset = 0;
-    private float glowCompensation = 1f;
-
-    public float smooth(float x) {
-        return 0.5f - ((float) (Math.cos(x * MathUtils.FPI) / 2));
-    }
+    boolean init = false;
+    ShipAPI parentShip;
+    ShipEngineControllerAPI.ShipEngineAPI actualEngine;
+    ShipAPI engineModule;
+    boolean startupError = false;
+    boolean currentlyDisabled = false;
+    float neutralAngle = 0;
+    float angularVelocity = 0;
+    float thrustLevel = 0;
     @Override
     public void advance(float amount, CombatEngineAPI engine, WeaponAPI weapon) {
-        if (!runOnce) {
-            runOnce = true;
-            ship = weapon.getShip();
+        if(!init){
+            init = true;
+            parentShip = weapon.getShip();
 
-            engines = ship.getEngineController();
-
-            ShipHullSpecAPI spec = Global.getSettings().getHullSpec("zea_dawn_ao_thruster_droneL");
-            ShipVariantAPI v = Global.getSettings().createEmptyVariant("zea_dawn_ao_thruster_droneL", spec);
-
-            thrusterDrone = Global.getCombatEngine().createFXDrone(v);
-            thrusterDrone.setLayer(CombatEngineLayers.ABOVE_SHIPS_LAYER);
-            thrusterDrone.setOwner(ship.getOriginalOwner());
-            thrusterDrone.setDrone(true);
-            thrusterDrone.getAIFlags().setFlag(ShipwideAIFlags.AIFlags.DRONE_MOTHERSHIP, 100000f, ship);
-            thrusterDrone.setCollisionClass(CollisionClass.NONE);
-            thrusterDrone.giveCommand(ShipCommand.SELECT_GROUP, null, 0);
-            Global.getCombatEngine().addEntity(thrusterDrone);
-
-            //find the ship engine associated with the deco thruster
-            for (ShipEngineControllerAPI.ShipEngineAPI e : ship.getEngineController().getShipEngines()) {
+            // pair the actual engine and the module to this script
+            for (ShipEngineControllerAPI.ShipEngineAPI e : parentShip.getEngineController().getShipEngines()) {
                 if (MathUtils.isWithinRange(e.getLocation(), weapon.getLocation(), 2)) {
-                    thruster = e;
+                    actualEngine = e;
+                }
+            }
+            for (ShipAPI e : parentShip.getChildModulesCopy()) {
+                if (MathUtils.isWithinRange(e.getLocation(), weapon.getLocation(), 2)) {
+                    engineModule = e;
                 }
             }
 
-            //desync the engines wobble
-            wobbleOffset = (float) (Math.random() * MathUtils.FPI);
-
-            //"rest" angle when not in use
-            neutralAngle = weapon.getSlot().getAngle();
-            //ideal aim angle to rotate the ship (allows free-form placement on the hull)
-            turnRightAngle = MathUtils.clampAngle(VectorUtils.getAngle(ship.getLocation(), weapon.getLocation()));
-            turnRightAngle = MathUtils.getShortestRotation(ship.getFacing(), turnRightAngle) + 90;
-            //is the thruster performant at turning the ship? Engines closer to the center of mass will concentrate more on dealing with changes of velocity.
-            thrustToTurn = smooth(MathUtils.getDistance(ship.getLocation(), weapon.getLocation()) / ship.getCollisionRadius());
-
-            maxAngleChangePerSecond = weapon.getTurnRate();
-            maxThrustChangePerSecond = weapon.getDerivedStats().getDps()/100f;
-        }
-
-        if (engine.isPaused() || ship.getOriginalOwner() == -1) {
-            return;
-        }
-
-        //check for death/engine disabled
-
-        if (!ship.isAlive()) {
-            if (thrusterDrone != null) {
-                Global.getCombatEngine().removeEntity(thrusterDrone);
+            if(actualEngine == null || engineModule == null){
+                startupError = true;
+                return;
             }
+
+            neutralAngle = weapon.getArcFacing();
+        }
+
+        if(startupError || !parentShip.isAlive()) return;
+
+
+        // apply stat boosts as needed
+        if (currentlyDisabled){
+            if (actualEngine.isActive() && !engineModule.getEngineController().isFlamedOut()){
+                currentlyDisabled = false;
+                engineOnline(weapon.getId() + "_engine_module_offline");
+            }
+            else{
+                engineOffline(weapon.getId() + "_engine_module_offline");
+                return;
+            }
+        }
+
+        // if module is dead, sync with actual engine
+        if (!engineModule.isAlive()) {
+            actualEngine.disable(true);
+            currentlyDisabled = true;
             return;
         }
 
-        if(!thruster.isActive()){
-            previousThrust = 0;
+        // disable engine if module is flamed out
+        if(engineModule.getEngineController().isFlamedOut() && !currentlyDisabled){
+            actualEngine.disable();
+            currentlyDisabled = true;
             return;
         }
+
+        // if actual engine is disabled, flameout the module
+        if(actualEngine.isDisabled() && !currentlyDisabled){
+            engineModule.getEngineController().forceFlameout();
+            currentlyDisabled = true;
+            return;
+        }
+
 
         //check what the ship is doing
-        float accelerateAngle = neutralAngle;
-        float turnAngle = neutralAngle;
-        float thrust = 0;
+        Float lateralThrustAngle = null;
+        ShipEngineControllerAPI engines = parentShip.getEngineController();
 
         if (engines.isAccelerating()) {
-            accelerateAngle = 180;
-            thrust = 1.5f;
-            accel = true;
+            if (engines.isStrafingLeft()) {
+                lateralThrustAngle = 225f;
+            } else if (engines.isStrafingRight()) {
+                lateralThrustAngle = 135f;
+            } else{
+                lateralThrustAngle = 180f;
+            }
         } else if (engines.isAcceleratingBackwards()) {
-            accelerateAngle = 0;
-            thrust = 1.5f;
-            accel = true;
-        } else if (engines.isDecelerating()) {
-            accelerateAngle = neutralAngle;
-            thrust = 0.5f;
-            accel = true;
-        } else {
-            accel = false;
-        }
-
-        if (engines.isStrafingLeft()) {
-            if (thrust == 0) {
-                accelerateAngle = -90;
-            } else {
-                accelerateAngle = MathUtils.getShortestRotation(accelerateAngle, -90) / 2 + accelerateAngle;
+            if (engines.isStrafingLeft()) {
+                lateralThrustAngle = 315f;
+            } else if (engines.isStrafingRight()) {
+                lateralThrustAngle = 45f;
+            } else{
+                lateralThrustAngle = 0f;
             }
-            thrust = Math.max(1, thrust);
-            accel = true;
+        } else if (engines.isStrafingLeft()) {
+            lateralThrustAngle = 270f;
         } else if (engines.isStrafingRight()) {
-            if (thrust == 0) {
-                accelerateAngle = 90;
-            } else {
-                accelerateAngle = MathUtils.getShortestRotation(accelerateAngle, 90) / 2 + accelerateAngle;
-            }
-            thrust = Math.max(1, thrust);
-            accel = true;
+            lateralThrustAngle = 90f;
         }
 
+        Float rotationalThrustAngle = null;
         if (engines.isTurningRight()) {
-            turnAngle = turnRightAngle;
-            thrust = Math.max(1, thrust);
-            turn = true;
+            rotationalThrustAngle = MathUtils.clampAngle(neutralAngle + 90);
         } else if (engines.isTurningLeft()) {
-            turnAngle = MathUtils.clampAngle(180 + turnRightAngle);
-            thrust = Math.max(1, thrust);
-            turn = true;
-        } else {
-            turn = false;
+            rotationalThrustAngle = MathUtils.clampAngle(neutralAngle - 90);
         }
 
-
-        //calculate the corresponding vector thrusting
-        if (thrust > 0) {
-            ship.getEngineController().forceShowAccelerating();
-            //SHIP.getEngineController().extendFlame(this, 2f, 0.5f, 0.5f);
-            //DEBUG
-            Vector2f offset = new Vector2f(weapon.getLocation().x - ship.getLocation().x, weapon.getLocation().y - ship.getLocation().y);
-            VectorUtils.rotate(offset, -ship.getFacing(), offset);
-            float thrustChangeModifier = (ship.getMutableStats().getTurnAcceleration().computeMultMod() + ship.getMutableStats().getAcceleration().computeMultMod()) / 2;
-
-            if (!turn) {
-                //thrust only, easy.
-                thrust(weapon, accelerateAngle, thrustChangeModifier, amount);
-
-            } else {
-                if (!accel) {
-                    //turn only, easy too.
-                    thrust(weapon, turnAngle, thrustChangeModifier, amount);
-
-
-                } else {
-                    //combined turn and thrust, aka the funky part.
-
-
-                    //start from the neutral angle
-                    float combinedAngle = neutralAngle;
-
-                    //adds both thrust and turn angle at their respective thrust-to-turn ratio. Gives a "middleground" angle
-                    combinedAngle = MathUtils.clampAngle(combinedAngle + MathUtils.getShortestRotation(neutralAngle, accelerateAngle));
-                    combinedAngle = MathUtils.clampAngle(combinedAngle + thrustToTurn * MathUtils.getShortestRotation(accelerateAngle, turnAngle));
-
-                    thrust(weapon, combinedAngle, thrustChangeModifier, amount);
-                }
-            }
-
-        } else {
-            thrust(weapon, neutralAngle, 0, amount);
+        if (lateralThrustAngle != null && rotationalThrustAngle != null){
+            float angleDifference = MathUtils.getShortestRotation(lateralThrustAngle, rotationalThrustAngle);
+            float thrustAngle = MathUtils.clampAngle(lateralThrustAngle + angleDifference * 0.5f); // 0.5 is magic number for half lateral and half rotational
+            thrust(weapon, thrustAngle, amount, true, Math.abs(angleDifference) > 120f);
+        } else if (lateralThrustAngle != null) {
+            thrust(weapon, lateralThrustAngle, amount, true, false);
+        } else if (rotationalThrustAngle != null) {
+            thrust(weapon, rotationalThrustAngle, amount, true, false);
+        } else{
+            thrust(weapon, neutralAngle, amount, false, false);
         }
 
+        weapon.getArc();
+        weapon.getArcFacing();
+
+        if (parentShip.getEngineController().isDecelerating()) {
+
+        }
     }
 
     public boolean isAngleWithinArc(float startAngle, float endAngle, float testAngle) {
@@ -202,69 +157,54 @@ public class DroneVectorThruster implements EveryFrameWeaponEffectPlugin {
         }
     }
 
-
-    private void thrust(WeaponAPI weapon, float angle, float thrustChangeModifier, float amount) {
-
+    private void thrust(WeaponAPI weapon, float angle, float amount, boolean accelerating, boolean flaring) {
         //target angle
-        float optimalAim = angle + ship.getFacing();
+        float optimalAim = MathUtils.clampAngle(angle + parentShip.getFacing());
+        float optimalInArc = MathUtils.clampAngle(Misc.isInArc(weapon.getArcFacing(), weapon.getArc(), angle) ? optimalAim :
+                weapon.getArcFacing() + parentShip.getFacing() + (MathUtils.getShortestRotation(weapon.getArcFacing(), angle) > 0 ? 1 : -1) * weapon.getArc()/2);
 
-        //how far from the target angle the engine is aimed at
 
-        float aimDirection = MathUtils.getShortestRotation(weapon.getCurrAngle(), optimalAim);
+        float shortestRotation = MathUtils.getShortestRotation(weapon.getCurrAngle(), optimalInArc);
+        boolean useShortestRotation = !Misc.isInArc(weapon.getCurrAngle() + shortestRotation/2,  Math.abs(shortestRotation), weapon.getArcFacing() + parentShip.getFacing()+180);
 
-        //thrust is reduced while the engine isn't facing the target angle
+        float angleDifference = useShortestRotation ? shortestRotation :( shortestRotation > 0) ? shortestRotation - 360f : shortestRotation + 360f;
 
-        float targetThrust = MathUtils.clamp((1 - (Math.abs(aimDirection) / 90)),0, 1);
-        if (thrustChangeModifier == 0) targetThrust = 0;
+        float angularAccel = weapon.getDerivedStats().getDps();
+        boolean angularDecel = Math.pow(angularVelocity, 2) / (2 * angularAccel) > Math.abs(angleDifference) && (angleDifference * angularVelocity) > 0;
+        angularVelocity = MathUtils.clamp(angularVelocity + (((angleDifference > 0) ^ angularDecel) ? angularAccel : -angularAccel) * amount, -weapon.getTurnRate(), weapon.getTurnRate());
 
-        float currentThrust;
-        if (previousThrust < targetThrust){
-            currentThrust = previousThrust + amount * maxThrustChangePerSecond;
-            if (currentThrust > targetThrust)
-                currentThrust = targetThrust;
+        float nextCurrAngle = weapon.getCurrAngle() + amount * angularVelocity;
+
+        if (Math.abs(nextCurrAngle - optimalInArc) < 0.01) angularVelocity = 0f;
+
+        engineModule.setFacing(nextCurrAngle+180);
+        weapon.setCurrAngle(nextCurrAngle);
+        actualEngine.getEngineSlot().setAngle(nextCurrAngle);
+
+        thrustLevel += (accelerating && Misc.isInArc(optimalAim, 120, nextCurrAngle) ? 1 : -1) * weapon.getRange()/100 * amount;
+        thrustLevel = Math.max(Math.min(1, thrustLevel), 0);
+        for (ShipEngineControllerAPI.ShipEngineAPI engine : engineModule.getEngineController().getShipEngines()){
+            engineModule.getEngineController().setFlameLevel(engine.getEngineSlot(), Misc.interpolate(0.8f,1, thrustLevel));
         }
-        else {
-            currentThrust = previousThrust - amount * maxThrustChangePerSecond * 2.5f;
-            if (currentThrust < targetThrust)
-                currentThrust = targetThrust;
-        }
-        currentThrust = MathUtils.clamp(currentThrust, 0, 1);
-        previousThrust = currentThrust;
+    }
 
-        //engine wobble
-        float targetAim = optimalAim + ((Math.abs(aimDirection) < 10f) ? (float) (2 * FastTrig.cos(ship.getFullTimeDeployed() * 5 + wobbleOffset)) : 0f);
-        aimDirection = MathUtils.getShortestRotation(weapon.getCurrAngle(), targetAim);
-        if ( isAngleWithinArc(weapon.getCurrAngle(), angle + ship.getFacing(), weapon.getArcFacing() + ship.getFacing() + 180))
-            aimDirection = -aimDirection;
+    public void engineOffline(String id){
+        MutableShipStatsAPI stats = parentShip.getMutableStats();
+        stats.getMaxSpeed().modifyMult(id, 0.8f);
+        stats.getMaxTurnRate().modifyMult(id, 0.8f);
+        stats.getAcceleration().modifyMult(id, 0.8f);
+        stats.getTurnAcceleration().modifyMult(id, 0.8f);
+        engineModule.getMutableStats().getCombatEngineRepairTimeMult().unmodify(id);
+        Global.getCombatEngine().maintainStatusForPlayerShip(id, "", "Engine Module Offline", "-20% Engine Stats", true);
+        thrustLevel = 0;
+    }
 
-        float turnBonus = Misc.interpolate(1, 2, Math.abs(aimDirection)/180);
-        if(aimDirection > 0){
-            weapon.setCurrAngle(MathUtils.clampAngle(weapon.getCurrAngle() + amount * maxAngleChangePerSecond * turnBonus));
-        } else{
-            weapon.setCurrAngle(MathUtils.clampAngle(weapon.getCurrAngle() - amount * maxAngleChangePerSecond * turnBonus));
-        }
-
-        EngineSlotAPI engineSlot = thruster.getEngineSlot();
-        float flameLevel = 0f;
-        if(currentThrust > 0) flameLevel = Misc.interpolate(0.0f, 1f, currentThrust);
-
-        ship.getEngineController().setFlameLevel(engineSlot, 0);
-        //((com.fs.starfarer.loading.specs.EngineSlot) engineSlot).setGlowParams(thruster.width * glowCompensation, thruster.length + offset,1f,1f); // no clue what v2 and v3 do
-        engineSlot.setAngle(weapon.getCurrAngle() - weapon.getShip().getFacing());
-        engineSlot.setGlowSizeMult(0f);
-
-
-        if (thrusterDrone != null) {
-
-            for(ShipEngineControllerAPI.ShipEngineAPI droneEngine : thrusterDrone.getEngineController().getShipEngines()){
-                EngineSlotAPI droneEngineSlot = droneEngine.getEngineSlot();
-                thrusterDrone.getEngineController().setFlameLevel(droneEngineSlot, flameLevel);
-            }
-
-            thrusterDrone.setOwner(ship.getOwner());
-            thrusterDrone.getLocation().set(weapon.getLocation());
-            thrusterDrone.setFacing(weapon.getCurrAngle()+180);
-            thrusterDrone.getVelocity().set(ship.getVelocity());
-        }
+    public void engineOnline(String id){
+        MutableShipStatsAPI stats = parentShip.getMutableStats();
+        stats.getMaxSpeed().unmodify(id);
+        stats.getMaxTurnRate().unmodify(id);
+        stats.getAcceleration().unmodify(id);
+        stats.getTurnAcceleration().unmodify(id);
+        engineModule.getMutableStats().getCombatEngineRepairTimeMult().modifyMult(id, 100);
     }
 }
