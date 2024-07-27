68479c0ac96948d18a5176e1c312e0424471373f
Lukas22041 <lukas.kurth04@gmail.com>
2024-07-28 01:35:06 +0200

    added background rendering element

---
+++ src/org/selkie/kol/hullmods/HullmodBackgroundElement.java
@@ -0,0 +1,35 @@
+package org.selkie.kol.hullmods;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.BaseCustomUIPanelPlugin;
+import com.fs.starfarer.api.graphics.SpriteAPI;
+import com.fs.starfarer.api.ui.CustomPanelAPI;
+import com.fs.starfarer.api.ui.PositionAPI;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class HullmodBackgroundElement extends BaseCustomUIPanelPlugin {
+
+
+    public SpriteAPI sprite;
+
+    public TooltipMakerAPI tooltip;
+
+    public HullmodBackgroundElement(TooltipMakerAPI tooltip, SpriteAPI sprite) {
+        CustomPanelAPI panel = Global.getSettings().createCustom(0f, 0f, this);
+        tooltip.addCustom(panel, 0f);
+
+        this.tooltip = tooltip;
+        this.sprite = sprite;
+    }
+
+    @Override
+    public void render(float alphaMult) {
+
+        sprite.setSize( tooltip.getWidthSoFar() + 20, tooltip.getHeightSoFar() + 10);
+        sprite.setAdditiveBlend();
+        sprite.render(tooltip.getPosition().getX(), tooltip.getPosition().getY());
+
+    }
+}
