16048b4c49edefc66fef957e584ec60b47c4509a
Starficz <starficz@gmail.com>
2024-10-19 15:39:50 -0400

    Logi Quest Branch

--- src/org/selkie/kol/campaign/missions/MGAIntro.kt
+++ src/org/selkie/kol/campaign/missions/MGAIntro.kt
@@ -2,7 +2,6 @@ package org.selkie.kol.campaign.missions
 
 import com.fs.starfarer.api.campaign.econ.MarketAPI
 import com.fs.starfarer.api.characters.PersonAPI
-import com.fs.starfarer.api.impl.campaign.ids.People
 import com.fs.starfarer.api.impl.campaign.missions.hub.HubMissionWithSearch
 import com.fs.starfarer.api.ui.TooltipMakerAPI
 import org.selkie.kol.helpers.KolStaticStrings
@@ -29,10 +28,11 @@ class MGAIntro : HubMissionWithSearch() {
         setStartingStage(Stage.VISIT_MGA_HEADQUARTERS)
         addSuccessStages(Stage.COMPLETED)
 
+        //Console.showMessage(Global.getSector().getMemoryWithoutUpdate().get("$gaRH_completed"))
         setStoryMission()
 
         makeImportant(sembene!!.market, null, Stage.VISIT_MGA_HEADQUARTERS)
-        setStageOnMemoryFlag(Stage.COMPLETED, sembene!!.market, "\$MGAIntro_completed")
+        setStageOnMemoryFlag(Stage.COMPLETED, sembene!!.market, "\$mgaIntro_completed")
 
         setRepFactionChangesNone()
         setRepPersonChangesNone()
@@ -46,20 +46,20 @@ class MGAIntro : HubMissionWithSearch() {
     override fun addDescriptionForNonEndStage(info: TooltipMakerAPI, width: Float, height: Float) {
         val opad = 10f
         if (currentStage == Stage.VISIT_MGA_HEADQUARTERS) {
-            info.addPara("Go to Nomios Rapid Autofab and enquire about the contract.", opad)
+            info.addPara("Go to Nomios Rapid Autofab and enquire about the Mbaye-Gogol contract Callisto Ibrahim has referred you to.", opad)
         }
     }
 
     override fun addNextStepText(info: TooltipMakerAPI, tc: Color?, pad: Float): Boolean {
         if (currentStage == Stage.VISIT_MGA_HEADQUARTERS) {
-            info.addPara("Enquire about the Contract.", tc, pad)
+            info.addPara("Enquire about the Contract", tc, pad)
             return true
         }
         return false
     }
 
     override fun getBaseName(): String {
-        return "Go to Nominos Rapid Autofab"
+        return "A New Opportunity"
     }
 
     override fun getPostfixForState(): String {
