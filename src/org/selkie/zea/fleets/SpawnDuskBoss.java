a2a327cad4cce71de6f8579b43d3b23940ad3627
nikothedude <simon.prouty@gmail.com>
2026-06-27 14:02:37 -0400

    yargh

--- src/org/selkie/zea/fleets/SpawnDuskBoss.java
+++ src/org/selkie/zea/fleets/SpawnDuskBoss.java
@@ -66,6 +66,7 @@ public class SpawnDuskBoss {
 		flagship.getVariant().addTag(Tags.SHIP_LIMITED_TOOLTIP);
 		flagship.getVariant().addTag(ZeaMemKeys.ZEA_BOSS_TAG); //Now confirmed by fleet rule.
 		flagship.getVariant().addTag(Tags.VARIANT_UNBOARDABLE); //Now confirmed by fleet rule.
+        flagship.getVariant().addTag(Tags.SHIP_RECOVERABLE); // counterintuitively makes the ship not appear in debris fields
 
 		flagship.getStats().getDynamic().getMod(Stats.INDIVIDUAL_SHIP_RECOVERY_MOD).modifyFlat("NoNormalRecovery", -2000);
 		flagship.getCaptain().setPortraitSprite(Global.getSettings().getSpriteName(GfxCat.CHARACTERS, ZeaStaticStrings.portraitDuskBoss));
